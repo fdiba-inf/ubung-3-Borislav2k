578672f8b5d88734664eccf95943ec219582c1e1
Borislav2k <borislav_bonev12@yahoo.com>
2020-10-13 16:13:44 +0000

    everything

---
+++ src/exercise3/NumberCounter.java
@@ -0,0 +1,22 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class NumberCounter {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int negCount = 0;
+        int positiveCount = 0;
+        double sum = 0;
+        int n = input.nextInt();
+        while (number! > 0)
+        if (n>0);
+{positiveCount++;       
+           sum = sum + number;
+            number = input.nextInt();
+         System.out.println("Count: " + count)    
+        System.out.println("sum" + number);
+    }
+
+}
---
+++ src/exercise3/PrimeNumber.java
@@ -0,0 +1,31 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+  
+public static void main(String args[])
+{		
+int temp;
+boolean isPrime=true;
+	
+System.out.println("Input number:");
+	
+int num = scan.nextInt();
+for(int i=2;i<=num/2;i++)
+{
+temp=num%i;
+if(temp==0)
+{
+isPrime=false;
+break;
+}
+}
+	if(isPrime)
+System.out.println(num + " is a Prime Number");
+	else
+System.out.println(num + " is not a Prime Number");
+}
+}
+
+   
